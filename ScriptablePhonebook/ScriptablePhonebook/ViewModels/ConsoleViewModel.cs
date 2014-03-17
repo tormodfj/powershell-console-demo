e8eff2a18d62f6ed41f929f6245a4244d9c384ae
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2014-03-17 16:40:00 +0100

    Connected console with PowerShell

--- ScriptablePhonebook/ScriptablePhonebook/ViewModels/ConsoleViewModel.cs
+++ ScriptablePhonebook/ScriptablePhonebook/ViewModels/ConsoleViewModel.cs
@@ -1,14 +1,73 @@
 ﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Caliburn.Micro;
+using ScriptablePhonebook.Automation;
 
 namespace ScriptablePhonebook.ViewModels
 {
     [Export(typeof(ConsoleViewModel))]
     public class ConsoleViewModel : Screen
     {
-        public ConsoleViewModel()
+        private readonly IPowerShellHelper psHelper;
+
+        private string input;
+        private string output;
+
+        [ImportingConstructor]
+        public ConsoleViewModel(IPowerShellHelper psHelper)
         {
+            this.psHelper = psHelper;
+
             DisplayName = "Phonebook Console";
+        }
+
+        public string Input
+        {
+            get
+            {
+                return input;
+            }
+            set
+            {
+                input = value;
+                NotifyOfPropertyChange(() => Input);
+            }
+        }
+
+        public string Output
+        {
+            get
+            {
+                return output;
+            }
+            set
+            {
+                output = value;
+                NotifyOfPropertyChange(() => Output);
+            }
+        }
+
+        public void Run()
+        {
+            var result = psHelper.ExecutePS(input);
+            Output += result;
+        }
+
+        public void Clear()
+        {
+            Output = string.Empty;
+            Input = string.Empty;
+        }
+
+        public void HandlePreviewKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.F5) Run();
+        }
+
+        public void ScrollToEnd(TextBox textBox)
+        {
+            textBox.ScrollToEnd();
         }
     }
 }
