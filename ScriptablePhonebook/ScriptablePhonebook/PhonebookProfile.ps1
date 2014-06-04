0eb970e8f74e7591bf5f397d4cfb93b9f14f02c3
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2014-06-04 23:11:29 +0200

    Exposed repository and added basic functions to profile

--- ScriptablePhonebook/ScriptablePhonebook/PhonebookProfile.ps1
+++ ScriptablePhonebook/ScriptablePhonebook/PhonebookProfile.ps1
@@ -1,1 +1,58 @@
-﻿
+﻿function New-Contact {
+    <#
+        .Synopsis
+            Creates a new contact
+        .Description
+            The New-Contact function creates a new phonebook contact and sets both name and number
+        .Example
+            New-Contact "John Doe" "99887766"
+    #>
+
+    param (
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string]$Name,
+
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string]$Number
+    )
+
+    $contact = New-Object ScriptablePhonebook.Models.Contact
+    
+    $contact.Name = $Name
+    $contact.Number = $Number
+
+    $contact
+}
+
+function Clear-Contacts {
+    <#
+        .Synopsis
+            Deletes all contacts in the phonebook
+        .Description
+            The Clear-Contacts function deletes all contacts from the phonebook
+        .Example
+            Clear-Contacts
+    #>
+
+    $repository.Clear()
+}
+
+function Add-Contact {
+    <#
+        .Synopsis
+            Adds contacts to the phonebook
+        .Description
+            The Add-Contact function adds contacts to the phonebook.
+        .Example
+            New-Contact "John Doe" "99887766" | Add-Contact
+        .Example
+            Add-Contact(New-Contact "John Doe" "99887766")
+    #>
+
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        $Contact
+    )
+
+    $repository.AddContact($Contact)
+}
--- ScriptablePhonebook/ScriptablePhonebook/Repositories/ContactRepository.cs
+++ ScriptablePhonebook/ScriptablePhonebook/Repositories/ContactRepository.cs
@@ -2,50 +2,53 @@
 using System.ComponentModel.Composition;
 using System.Linq;
 using Caliburn.Micro;
+using ScriptablePhonebook.Automation;
 using ScriptablePhonebook.Models;
 
 namespace ScriptablePhonebook.Repositories
 {
-    [Export(typeof(IContactRepository))]
-    public class ContactRepository : IContactRepository
-    {
-        private readonly Contact[] sampleData =
-        {
-            new Contact { Name="Jens Stoltenberg", Number="12345678" },
-            new Contact { Name="Erna Solberg", Number="44332211" },
-            new Contact { Name="Siv Jensen", Number="98765432" },
-        };
-        private readonly IList<Contact> contacts;
-        private readonly IEventAggregator eventAggregator;
+	[Export(typeof(IContactRepository))]
+	public class ContactRepository : IContactRepository
+	{
+		private readonly Contact[] sampleData =
+		{
+			new Contact { Name="Jens Stoltenberg", Number="12345678" },
+			new Contact { Name="Erna Solberg", Number="44332211" },
+			new Contact { Name="Siv Jensen", Number="98765432" },
+		};
+		private readonly IList<Contact> contacts;
+		private readonly IEventAggregator eventAggregator;
 
-        [ImportingConstructor]
-        public ContactRepository(IEventAggregator eventAggregator)
-        {
-            this.contacts = new List<Contact>(sampleData);
-            this.eventAggregator = eventAggregator;
-        }
+		[ImportingConstructor]
+		public ContactRepository(IEventAggregator eventAggregator, IPowerShellConfig psConfig)
+		{
+			this.contacts = new List<Contact>(sampleData);
+			this.eventAggregator = eventAggregator;
 
-        public IEnumerable<Contact> GetAll()
-        {
-            return contacts.ToArray();
-        }
+			psConfig.SetVariable("repository", this);
+		}
 
-        public void Clear()
-        {
-            contacts.Clear();
-            eventAggregator.Publish(new ContactRepositoryModifiedMessage());
-        }
+		public IEnumerable<Contact> GetAll()
+		{
+			return contacts.ToArray();
+		}
 
-        public void AddContact(Contact contact)
-        {
-            contacts.Add(contact);
-            eventAggregator.Publish(new ContactRepositoryModifiedMessage());
-        }
+		public void Clear()
+		{
+			contacts.Clear();
+			eventAggregator.Publish(new ContactRepositoryModifiedMessage());
+		}
 
-        public void RemoveContact(Contact contact)
-        {
-            contacts.Remove(contact);
-            eventAggregator.Publish(new ContactRepositoryModifiedMessage());
-        }
-    }
+		public void AddContact(Contact contact)
+		{
+			contacts.Add(contact);
+			eventAggregator.Publish(new ContactRepositoryModifiedMessage());
+		}
+
+		public void RemoveContact(Contact contact)
+		{
+			contacts.Remove(contact);
+			eventAggregator.Publish(new ContactRepositoryModifiedMessage());
+		}
+	}
 }
